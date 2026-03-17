be3a49f87bcebe7df38d8e6ad5bf106546ae0de3
Aditya Raj <89096015+ADITYA0018TH@users.noreply.github.com>
2026-03-17 12:03:03 +0530

    feat: initialize React project with Vite, add essential files and configurations
    - Created package.json with dependencies and scripts for development
    - Added favicon.svg and icons.svg for application branding
    - Implemented basic App component in App.jsx
    - Set up main entry point in main.jsx for rendering the App
    - Configured Vite with react plugin in vite.config.js

---
+++ 01basic/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Aditya | basic react app</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>Aditya Raj</h1>
+  )
+}
+
+export default App
