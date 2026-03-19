0a0540ccaaa8abbd1791e491e26e2df33201498a
Aditya Raj <89096015+ADITYA0018TH@users.noreply.github.com>
2026-03-19 17:40:46 +0530

    feat: update App component to display user name and add custom render functionality

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,11 @@
 import Exp from "./Exp";
 function App() {
-
+  const name = "Aditya";
   return (
     <>
     <Exp />
+    <h1>My name is {name}</h1>
+    <p>Exp is ok</p>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,42 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <div>
+      <h1>My React App!</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit Google'
+// }
+
+// const anotherElement = (
+//   <a href="https://www.google.com" target="_blank">
+//     Click me to visit Google
+//   </a>
+// )
+const anotherUser = "Exp is working";
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://www.google.com',
+    target: '_blank'
+  },
+  'Click me to visit Google',
+  anotherUser
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  reactElement
 )
